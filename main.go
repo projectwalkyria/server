b98412cbdfea21d32be716fafaa08ec72a1d2c40
Pedro Renan Mussato <81251621+PedroMussato@users.noreply.github.com>
2024-09-09 16:10:24 -0300

    default port

--- main.go
+++ main.go
@@ -12,13 +12,13 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
-
 	// Create Table
-	createTable(db)
 
-    http.HandleFunc("POST /con/{id}", conPost)
-    http.HandleFunc("PUT /con/{id}", conPut)
-    http.HandleFunc("GET /con/{id}", conGet)
-    http.HandleFunc("DELETE /con/{id}", conDelete)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	createTable(db)
+	
+	http.HandleFunc("POST /con/{id}", conPost)
+	http.HandleFunc("PUT /con/{id}", conPut)
+	http.HandleFunc("GET /con/{id}", conGet)
+	http.HandleFunc("DELETE /con/{id}", conDelete)
+	log.Fatal(http.ListenAndServe(":53072", nil))
 }
